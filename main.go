ecb3e61d7a4d1f62c0a325cd016d11a86ff95add
Paulo <pauloeduardo2709@gmail.com>
2021-03-30 00:55:21 -0300

    Add utils and logger

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Pauloo27/aryzona/logger"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	logger.HandleFatal(err, "Cannot load .env")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	logger.Success("Hello World")
+	logger.Debug("Hello World")
+	logger.Info("Hello World")
+	logger.Warn("Hello World")
+	logger.Error("Hello World")
 }
