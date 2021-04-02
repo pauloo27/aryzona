b04e2b530fc753055f0ca5ef7e3c5ee2bfd1ae44
Paulo <pauloeduardo2709@gmail.com>
2021-04-02 00:37:29 -0300

    Fix presence message

--- discord/listeners/ready.go
+++ discord/listeners/ready.go
@@ -3,10 +3,9 @@ package listeners
 import (
 	"os"
 
-	"github.com/Pauloo27/aryzona/utils"
 	"github.com/bwmarrin/discordgo"
 )
 
 func Ready(s *discordgo.Session, m *discordgo.Ready) {
-	s.UpdateStreamingStatus(0, utils.Fmt("last commit: %s", os.Getenv("DC_BOT_PRESENCE")), "https://twitch.tv/gaules")
+	s.UpdateStreamingStatus(0, os.Getenv("DC_BOT_PRESENCE"), "https://twitch.tv/gaules")
 }
