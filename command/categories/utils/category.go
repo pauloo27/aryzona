3035633859daeeb5dfc9d0efb65e96b5adea0abb
Paulo <pauloeduardo2709@gmail.com>
2021-05-11 00:10:55 -0300

    Add floof command

--- command/categories/utils/category.go
+++ command/categories/utils/category.go
@@ -5,6 +5,6 @@ import "github.com/Pauloo27/aryzona/command"
 var Utils = command.Category{
 	Name: "Utils",
 	Commands: []*command.Command{
-		&PingCommand, &UptimeCommand, &HelpCommand, &WoofCommand, &MeowCommand,
+		&PingCommand, &UptimeCommand, &HelpCommand, &WoofCommand, &MeowCommand, &FloofCommand,
 	},
 }
