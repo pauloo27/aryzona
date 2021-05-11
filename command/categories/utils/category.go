89b844f49aa527e41d67ecc130f75afab115d459
Paulo <pauloeduardo2709@gmail.com>
2021-05-10 23:48:03 -0300

    Add meow command to command list

--- command/categories/utils/category.go
+++ command/categories/utils/category.go
@@ -3,6 +3,8 @@ package utils
 import "github.com/Pauloo27/aryzona/command"
 
 var Utils = command.Category{
-	Name:     "Utils",
-	Commands: []*command.Command{&PingCommand, &UptimeCommand, &HelpCommand, &WoofCommand},
+	Name: "Utils",
+	Commands: []*command.Command{
+		&PingCommand, &UptimeCommand, &HelpCommand, &WoofCommand, &MeowCommand,
+	},
 }
