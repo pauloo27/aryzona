376ff9264d11ad8865465a978d03b9e648689c63
Paulo <pauloeduardo2709@gmail.com>
2021-05-15 21:43:58 -0300

    Improve presence message

--- discord/listeners/ready.go
+++ discord/listeners/ready.go
@@ -42,7 +42,7 @@ func createStartedEmbed() *discordgo.MessageEmbed {
 func Ready(s *discordgo.Session, m *discordgo.Ready) {
 	presence := os.Getenv("DC_BOT_PRESENCE")
 	if presence == "" {
-		presence = git.CommitMessage
+		presence = utils.Fmt("Last commit: %s", git.CommitMessage)
 	}
 	s.UpdateStreamingStatus(0, presence, "https://twitch.tv/gaules")
 
