dc130bd5f1107d2e2987eb80b3f20f937845f96f
Paulo <pauloeduardo2709@gmail.com>
2021-06-20 16:53:33 -0300

    Add utils.Wrap() to wrap errors

--- utils/error.go
+++ utils/error.go
@@ -1,7 +1,13 @@
 package utils
 
+import "errors"
+
 func HandleFatal(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Wrap(msg string, err error) error {
+	return errors.New(Fmt("%s: %v", msg, err))
 }
