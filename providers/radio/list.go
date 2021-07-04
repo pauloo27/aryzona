8188cb33e4654d644608e931d6fee7848eea34c4
Paulo <pauloeduardo2709@gmail.com>
2021-07-04 13:52:52 -0300

    Add lofi radio

--- providers/radio/list.go
+++ providers/radio/list.go
@@ -106,6 +106,12 @@ var radios = []*RadioChannel{
 		URL:  "http://lainon.life:8000/cafe.mp3",
 		Type: M3uRadio,
 	},
+	{
+		Id:   "lofi",
+		Name: "Lofi: beats to relax/study",
+		URL:  "https://youtube.com/watch?v=5qap5aO4i9A",
+		Type: YTLive,
+	},
 }
 
 func GetRadioList() []*RadioChannel {
