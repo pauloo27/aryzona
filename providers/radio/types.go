36efff3c0444a7c183abc3149326b783054a4285
Paulo <pauloeduardo2709@gmail.com>
2021-07-04 13:52:47 -0300

    Add youtube live radio type

--- providers/radio/types.go
+++ providers/radio/types.go
@@ -1,9 +1,28 @@
 package radio
 
+import (
+	"github.com/Pauloo27/aryzona/logger"
+	"github.com/Pauloo27/aryzona/providers/youtube"
+)
+
 var M3uRadio = &RadioType{
-	Name:    "m3ue playlist",
+	Name:    "m3u8 playlist",
 	IsOppus: false,
 	GetDirectURL: func(url string) string {
+		return url
+	},
+}
+
+var YTLive = &RadioType{
+	Name:    "YouTube live",
+	IsOppus: false,
+	GetDirectURL: func(url string) string {
+		url, err := youtube.GetLiveURL(url)
+
+		if err != nil {
+			logger.Errorf("%s", err)
+		}
+
 		return url
 	},
 }
