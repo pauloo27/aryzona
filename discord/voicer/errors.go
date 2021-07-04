6123bbd668945e46bd3c6e8af29d34bf69640cee
Paulo <pauloeduardo2709@gmail.com>
2021-07-04 14:13:08 -0300

    Reformat code

--- discord/voicer/errors.go
+++ discord/voicer/errors.go
@@ -3,5 +3,8 @@ package voicer
 import "github.com/Pauloo27/aryzona/utils"
 
 var (
-	ERR_ALREADY_PLAYING = utils.Errore{"ALREADY_PLAYING", "Already playing something in the current voice channel"}
+	ERR_ALREADY_PLAYING = utils.Errore{
+		ID:      "ALREADY_PLAYING",
+		Message: "Already playing something in the current guild",
+	}
 )
