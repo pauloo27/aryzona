486f79c00996ce6e09ad5d75b2aa82487ec85b66
Paulo <pauloeduardo2709@gmail.com>
2021-07-04 13:52:13 -0300

    Add support to livestreams

--- providers/youtube/youtube.go
+++ providers/youtube/youtube.go
@@ -1,8 +1,12 @@
 package youtube
 
 import (
+	"io"
+	"net/http"
 	"strings"
 
+	"github.com/Pauloo27/aryzona/logger"
+	"github.com/Pauloo27/aryzona/utils"
 	"github.com/kkdai/youtube/v2"
 )
 
@@ -17,4 +21,48 @@ func GetMediaURL(url string) (string, error) {
 
 	// 140 = audio
 	return client.GetStreamURL(video, video.Formats.FindByItag(140))
+}
+
+func getFirstURL(manifestURL string) (string, error) {
+	logger.Debugf("manifest %s", manifestURL)
+	res, err := http.Get(manifestURL)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	buffer, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	body := string(buffer)
+	logger.Debugf("body: \n%s", body)
+
+	for _, line := range strings.Split(body, "\n") {
+		if strings.HasPrefix(line, "https://") {
+			logger.Debugf("line =%s=", line)
+			return line, nil
+		}
+	}
+
+	return "", utils.Errore{ID: "URL_NOT_FOUND", Message: "URL not found"}
+}
+
+func GetLiveURL(url string) (string, error) {
+	videoID := strings.Split(url, "=")[1]
+	client := youtube.Client{}
+
+	video, err := client.GetVideo(videoID)
+	if err != nil {
+		return "", nil
+	}
+
+	manifest := video.HLSManifestURL
+	if manifest == "" {
+		return "", utils.Errore{
+			ID:      "HLS_NOT_FOUND",
+			Message: "HLS manifest not found",
+		}
+	}
+	return getFirstURL(manifest)
 }
