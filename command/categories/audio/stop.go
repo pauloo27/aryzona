fdbb949854882e96c9ef0626a4e2797518a7c41a
Paulo <pauloeduardo2709@gmail.com>
2021-08-09 20:33:22 -0300

    feat: add stop command

---
+++ command/categories/audio/stop.go
@@ -0,0 +1,25 @@
+package audio
+
+import (
+	"github.com/Pauloo27/aryzona/command"
+	"github.com/Pauloo27/aryzona/discord/voicer"
+	"github.com/Pauloo27/aryzona/logger"
+)
+
+var StopCommand = command.Command{
+	Name: "stop", Aliases: []string{"st", "parar", "pare"},
+	Handler: func(ctx *command.CommandContext) {
+		voicer := voicer.GetExistingVoicerForGuild(ctx.Message.GuildID)
+		if voicer == nil {
+			ctx.Error("Bot is not connect to a voice channel")
+			return
+		}
+		err := voicer.Disconnect()
+		if err != nil {
+			logger.Errorf("%v", err)
+			ctx.Error("Something went wrong when disconnecting...")
+			return
+		}
+		ctx.Success("Stopped!")
+	},
+}
