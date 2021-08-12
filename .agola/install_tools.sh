7f8cfa5cfa10b2d885a75e3856ac67cb6eceae53
Paulo <pauloeduardo2709@gmail.com>
2021-08-11 23:04:03 -0300

    ci: add inspect code rule

---
+++ .agola/install_tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Installl inspect tools
+
+# TODO: use a function or something
+[ ! -f "$GOPATH/bin/revive" ] && echo "Revive not found, installing..." && go install github.com/mgechev/revive@latest
+[ ! -f "$GOPATH/bin/misspell" ] && echo "misspell not found, installing..." && go install github.com/client9/misspell/cmd/misspell@latest
+[ ! -f "$GOPATH/bin/gosec" ] && echo "Gosec not found, installling..." && go install github.com/securego/gosec/v2/cmd/gosec@latest
+[ ! -f "$GOPATH/bin/staticcheck" ] && echo "Staticcheck not found, installling..." && go install honnef.co/go/tools/cmd/staticcheck@latest
+
+
