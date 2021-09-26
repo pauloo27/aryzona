e7a1411f3dad3971e47f344288b5291c5fdca8f2
Paulo <pauloeduardo2709@gmail.com>
2021-09-26 18:35:27 -0300

    improve: [wip] slash command handler

--- command/slash/slash.go
+++ command/slash/slash.go
@@ -32,12 +32,39 @@ func RegisterCommands() error {
 			logger.Error("Invalid slash command interaction received:", i.ApplicationCommandData().Name)
 			return
 		}
+
 		var args []string
 		for _, option := range i.ApplicationCommandData().Options {
 			args = append(args, option.StringValue())
 		}
+
+		var authorID string
+
+		if i.Member == nil {
+			authorID = i.User.ID
+		} else {
+			authorID = i.Member.User.ID
+		}
+
 		event := command.Event{
-			Message: i.Message,
+			AuthorID: authorID,
+			GuildID:  i.GuildID,
+			Reply: func(message string) error {
+				return s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: message,
+					},
+				})
+			},
+			ReplyEmbed: func(embed *discordgo.MessageEmbed) error {
+				return s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Embeds: []*discordgo.MessageEmbed{embed},
+					},
+				})
+			},
 		}
 		command.HandleCommand(commandName, args, s, &event)
 	})
