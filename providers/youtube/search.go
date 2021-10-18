44cbad4e0963a42977983335dd5d1d9bc62639e1
Paulo <git@pauloo27.dev>
2021-10-18 19:12:54 -0300

    help: regex =(

--- providers/youtube/search.go
+++ providers/youtube/search.go
@@ -7,6 +7,11 @@ import (
 	"github.com/Pauloo27/searchtube"
 )
 
+// FIXME: this regex allows things like `youtu.be.com`, which is not valid...
+// with a "right" string payload (and the hard part, the proper domain), the
+// regex might say that the link is from  youtube when it's not, leading the
+// bot to connect in a "invalid" server which can lead to "IP leaking" or
+// whatever...
 var videoRegex = regexp.MustCompile(`(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?`)
 
 func GetBestResult(searchQuery string) (string, error) {
