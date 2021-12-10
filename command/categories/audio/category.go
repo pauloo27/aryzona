33e796ff3bc2cd17fa59e23675ee444059656457
Paulo <git@pauloo27.dev>
2021-12-10 19:37:24 -0300

    feat: search youtube on startup to avoid ban by inactivity

--- command/categories/audio/category.go
+++ command/categories/audio/category.go
@@ -1,6 +1,12 @@
 package audio
 
-import "github.com/Pauloo27/aryzona/command"
+import (
+	"math/rand"
+	"time"
+
+	"github.com/Pauloo27/aryzona/command"
+	"github.com/Pauloo27/aryzona/providers/youtube"
+)
 
 var Audio = command.CommandCategory{
 	Name:  "Audio related stuff",
@@ -11,6 +17,28 @@ var Audio = command.CommandCategory{
 	},
 }
 
+var (
+	coolVids = []string{
+		"Baby Shark",
+		"Despacito",
+		"Johny Jonhy",
+		"Shape of you",
+		"See you again",
+		"Bath song",
+		"Uptime funk open suse",
+		"Dame To Cosita",
+		"Sorry",
+		"Lean On",
+		"Gangnam Style",
+	}
+)
+
 func init() {
 	command.RegisterCategory(Audio)
+	// so, youtube ban apikeys with more than 90 days of inactivity,
+	// since the bot is not used enought that i can asure that's never going to
+	// happen, let's search something on every startup
+	rand.Seed(time.Now().UnixNano())
+	index := rand.Intn(len(coolVids))
+	_, _, _ = youtube.GetBestResult(coolVids[index])
 }
