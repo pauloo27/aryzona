546704de2633e9d1fa556ea5fa0522fe87859b76
Paulo <git@pauloo27.dev>
2021-12-19 19:39:26 -0300

    ci: build docker image on deploy stage
    rename secrets/variables

--- .agola/update_variables.sh
+++ .agola/update_variables.sh
@@ -3,18 +3,18 @@
 PROJECT_ID=$1
 CI_URL=$2
 TOKEN=$3
+SECRET_NAME=REGISTRY
 
-agola project secret delete --project $PROJECT_ID -u $CI_URL --name DEPLOY --token $TOKEN
 
-agola project secret create --project $PROJECT_ID -u $CI_URL --name DEPLOY --token $TOKEN -f ./deploy-secrets.yml
+agola project secret delete --project $PROJECT_ID -u $CI_URL --name $SECRET_NAME --token $TOKEN
+agola project secret create --project $PROJECT_ID -u $CI_URL --name $SECRET_NAME --token $TOKEN -f ./secrets.yml
 
-function recreate_secret {
+function recreate_var {
   agola project variable delete --project $PROJECT_ID -u $CI_URL --name $1 --token $TOKEN
-  echo "- secret_name: DEPLOY
-  secret_var: $2" | agola project variable create --project $PROJECT_ID -u $CI_URL --name $1 --token $TOKEN -f "-"
+  echo "- secret_name: $SECRET_NAME
+  secret_var: $1" | agola project variable create --project $PROJECT_ID -u $CI_URL --name $1 --token $TOKEN -f "-"
 }
 
-recreate_secret discordwebhook DISCORD_WEBHOOK
-recreate_secret deployuser USER
-recreate_secret deployhost HOST
-recreate_secret deploypath PATH
+recreate_var registry-url
+recreate_var registry-image
+recreate_var registry-token
