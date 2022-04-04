881f9a028c90d9cf83bb89ccf147470b7e877e95
Paulo <git@pauloo27.dev>
2022-04-04 07:48:27 -0300

    fix(cmd): handle not commit info @ !uptime

--- internal/command/categories/utils/uptime.go
+++ internal/command/categories/utils/uptime.go
@@ -16,21 +16,23 @@ var UptimeCommand = command.Command{
 	Aliases: []string{"up"},
 	Handler: func(ctx *command.CommandContext) {
 		uptime := time.Since(*discord.Bot.StartedAt())
-		ctx.SuccessEmbed(
-			discord.NewEmbed().
-				WithTitle("Bot uptime").
-				WithField(":timer: Uptime", utils.FormatDuration(uptime)).
-				WithField(":gear: Implementation", discord.Bot.Implementation()).
-				WithField(
-					":floppy_disk: Last commit", fmt.Sprintf("[%s (%s)](%s/commit/%s)",
-						git.CommitMessage, git.CommitHash[:10], git.RemoteRepo, git.CommitHash),
-				).
-				WithField(
-					":computer: System info",
-					utils.Fmt("%s %s %s",
-						runtime.GOOS, runtime.GOARCH, runtime.Version(),
-					)).
-				WithField(":star: Started at", discord.Bot.StartedAt().Format("2 Jan, 15:04")),
-		)
+		embed := discord.NewEmbed().
+			WithTitle("Bot uptime").
+			WithField(":timer: Uptime", utils.FormatDuration(uptime)).
+			WithField(":gear: Implementation", discord.Bot.Implementation()).
+			WithField(
+				":computer: System info",
+				utils.Fmt("%s %s %s",
+					runtime.GOOS, runtime.GOARCH, runtime.Version(),
+				)).
+			WithField(":star: Started at", discord.Bot.StartedAt().Format("2 Jan, 15:04"))
+
+		if git.CommitHash != "" {
+			embed.WithField(
+				":floppy_disk: Last commit", fmt.Sprintf("[%s (%s)](%s/commit/%s)",
+					git.CommitMessage, git.CommitHash[:10], git.RemoteRepo, git.CommitHash),
+			)
+		}
+		ctx.SuccessEmbed(embed)
 	},
 }
