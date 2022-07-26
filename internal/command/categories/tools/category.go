25cc0afde305b6eb513cc6c4cde558662653691b
Paulo <git@pauloo27.dev>
2022-07-26 15:06:37 -0300

    fix(cmd): register tool commands category

--- internal/command/categories/tools/category.go
+++ internal/command/categories/tools/category.go
@@ -3,9 +3,11 @@ package tools
 import "github.com/Pauloo27/aryzona/internal/command"
 
 var Tools = command.CommandCategory{
-	Name:     "Tools",
-	Emoji:    "🔧",
-	Commands: []*command.Command{},
+	Name:  "Tools",
+	Emoji: "🔧",
+	Commands: []*command.Command{
+		&UUIDCommand, &CPFCommand, &CNPJCommand,
+	},
 }
 
 func init() {
