59a44a0e1840760cc89859388947cbc548b1b0ec
Paulo <git@pauloo27.dev>
2022-08-10 20:43:49 -0300

    ci: enable deploy for dev
    to a different docker image...

--- .agola/update_variables.sh
+++ .agola/update_variables.sh
@@ -5,14 +5,23 @@ CI_URL=$2
 TOKEN=$3
 SECRET_NAME=REGISTRY
 
-
-agola project secret delete --project $PROJECT_ID -u $CI_URL --name $SECRET_NAME --token $TOKEN
-agola project secret create --project $PROJECT_ID -u $CI_URL --name $SECRET_NAME --token $TOKEN -f ./secrets.yml
+agola project secret delete --project $PROJECT_ID -u $CI_URL --name $SECRET_NAME-dev --token $TOKEN
+agola project secret delete --project $PROJECT_ID -u $CI_URL --name $SECRET_NAME-prod --token $TOKEN
+agola project secret create --project $PROJECT_ID -u $CI_URL --name $SECRET_NAME-dev --token $TOKEN -f ./secrets.dev.yml
+agola project secret create --project $PROJECT_ID -u $CI_URL --name $SECRET_NAME-prod --token $TOKEN -f ./secrets.prod.yml
 
 function recreate_var {
   agola project variable delete --project $PROJECT_ID -u $CI_URL --name $1 --token $TOKEN
-  echo "- secret_name: $SECRET_NAME
-  secret_var: $1" | agola project variable create --project $PROJECT_ID -u $CI_URL --name $1 --token $TOKEN -f "-"
+    echo "
+  - secret_name: $SECRET_NAME-dev
+    secret_var: $1
+    when:
+      branch: dev
+  - secret_name: $SECRET_NAME-prod
+    secret_var: $1
+    when:
+      branch: master
+" | agola project variable create --project $PROJECT_ID -u $CI_URL --name $1 --token $TOKEN -f "-"
 }
 
 recreate_var registry-url
