e6fad19eae2480ee8fad86189e81adaf5c0ff6f9
Mauricio F <mauriciofsnts@gmail.com>
2023-02-03 21:01:39 -0300

    feat: i18n some commands

--- internal/command/categories/audio/skip.go
+++ internal/command/categories/audio/skip.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Pauloo27/aryzona/internal/command"
 	"github.com/Pauloo27/aryzona/internal/command/validations"
 	"github.com/Pauloo27/aryzona/internal/discord/voicer"
+	"github.com/Pauloo27/aryzona/internal/i18n"
 )
 
 var SkipCommand = command.Command{
@@ -11,9 +12,10 @@ var SkipCommand = command.Command{
 	Description: "Skip current item in the queue",
 	Validations: []*command.CommandValidation{validations.MustBePlaying},
 	Handler: func(ctx *command.CommandContext) {
+		t := ctx.T.(*i18n.CommandSkip)
 		vc := ctx.Locals["vc"].(*voicer.Voicer)
 
 		vc.Skip()
-		ctx.Success("Skipped!")
+		ctx.Success(t.Skipped.Str())
 	},
 }
