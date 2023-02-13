3d0a500aed3576e38e8a8e5d72c333dcd14af03b
Paulo <git@pauloo27.dev>
2023-02-12 21:39:06 -0300

    rev: "ci: remove "exit 0" from the install script"
    This reverts commit 5735152b7e9e5e8b7668640de690e808afe56867.

--- .agola/install_tools.sh
+++ .agola/install_tools.sh
@@ -5,3 +5,6 @@
 [ ! -f "$GOPATH/bin/revive" ] && echo "Revive not found, installing..." && go install github.com/mgechev/revive@latest
 [ ! -f "$GOPATH/bin/gosec" ] && echo "Gosec not found, installling..." && go install github.com/securego/gosec/v2/cmd/gosec@latest
 [ ! -f "$GOPATH/bin/staticcheck" ] && echo "Staticcheck not found, installling..." && go install honnef.co/go/tools/cmd/staticcheck@latest
+
+exit 0
+
