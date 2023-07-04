8c2e6eb92939aea880f8147402bf485a5426f863
Paulo <git@pauloo27.dev>
2023-07-04 03:27:44 -0300

    rev: "feat: use external base url in router"
    This reverts commit 7fbc3f53cb1c8135def40766f311fec90aeeeb66.

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"net/url"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -21,25 +20,13 @@ func StartHTTPServer() error {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Logger)
 
-	uri, err := url.Parse(config.Config.HTTPServerExternalURL)
-	if err != nil {
-		return err
-	}
-
-	baseRoute := uri.Path
-	if baseRoute == "" {
-		baseRoute = "/"
-	}
-
-	r.Route(baseRoute, func(r chi.Router) {
-		route(r)
-	})
+	route(r)
 
 	server := &http.Server{
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		Handler:      r,
-		Addr:         fmt.Sprintf(":%d", config.Config.HTTPServerPort),
+		Addr: fmt.Sprintf(":%d", config.Config.HTTPServerPort),
 	}
 
 	return server.ListenAndServe()
