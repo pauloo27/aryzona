b4d44b5b2f0d0b5180bc13305400474a1a65edcf
Paulo <git@pauloo27.dev>
2023-11-19 14:12:33 -0300

    doc: fair question

--- cmd/aryzona/main.go
+++ cmd/aryzona/main.go
@@ -7,6 +7,7 @@ var (
 )
 
 // the average American child is given $3.70 per tooth that falls out.
+// what is the tooth fairy's annual budget?
 
 func main() {
 	bootstrap.Start(commitHash, commitMessage)
