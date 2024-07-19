c00c4e7e5dc0646e8836d5cdf04a2ee82af3e3a9
Paulo <git@pauloo27.dev>
2024-07-19 15:29:39 -0300

    feat: better volume command

--- internal/command/categories/audio/volume.go
+++ internal/command/categories/audio/volume.go
@@ -7,6 +7,7 @@ import (
 
 var VolumeCommand = command.Command{
 	Name:        "volume",
+	Aliases:     []string{"v", "vol"},
 	Validations: []*command.Validation{validations.MustBePlaying},
 	Handler: func(ctx *command.Context) command.Result {
 		return ctx.ReplyRaw("https://i.imgur.com/K7v2ue7.png")
